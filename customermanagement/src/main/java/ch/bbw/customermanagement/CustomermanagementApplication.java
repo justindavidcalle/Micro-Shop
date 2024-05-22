4478809a3c9e7a135bfa4bbf90e06b376ae37e77
justindavidcalle <justincalle@outlook.de>
2024-05-22 16:00:09 +0200

    initials commit of every service

---
+++ customermanagement/src/main/java/ch/bbw/customermanagement/CustomermanagementApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.customermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomermanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomermanagementApplication.class, args);
+    }
+
+}
---
+++ payment/src/test/java/ch/bbw/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
